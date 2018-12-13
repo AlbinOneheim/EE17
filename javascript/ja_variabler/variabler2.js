1888e6668f11a74f4b4b9ffac199d365721ade02
Albin Oneheim <albin.oneheim@learnet.se>
2018-12-13 14:22:12 +0100

    addade lite mera knappar på sidan

--- javascript/ja_variabler/variabler2.js
+++ javascript/ja_variabler/variabler2.js
@@ -4,8 +4,10 @@ function start()
 {
     const eKnapp1 = document.querySelector("#knapp1");
     const eKnapp2 = document.querySelector("#knapp2");
+    const eKnapp3 = document.querySelector("#knapp3");
+    const eKnapp4 = document.querySelector("#knapp4");
+    const eKnapp5 = document.querySelector("#knapp5");
     const eTalet = document.querySelector("#talet");
-    const eKnapp3 = document.querySelector("#knapp3")
 
     eKnapp1.addEventListener("click", raknaUp);
     function raknaUp()
@@ -35,5 +37,25 @@ function start()
         let summaLaggTill = talet + 9000;
 
         eTalet.value = summaLaggTill;
+    }
+
+    eKnapp4.addEventListener("click", nollStall);
+    function nollStall()
+    {
+        let talet = Number(eTalet.value);
+
+        let startaOm = talet * 0;
+
+        eTalet.value = startaOm;
+    }
+
+    eKnapp5.addEventListener("click", taBort);
+    function taBort()
+    {
+        let talet = Number(eTalet.value);
+
+        let taBort = talet - 9000;
+
+        eTalet.value = taBort;
     }
 }
